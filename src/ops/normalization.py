ff5f957a065d4e13adea367d2e520a18c4a31de2
wuyuanhao <yuanhaowu@foxmail.com>
2025-12-08 21:51:31 +0800

    add bn forward

--- src/ops/normalization.py
+++ src/ops/normalization.py
@@ -20,6 +20,23 @@ def BatchNorm(x,gamma,beta,eps=1e-5):
     var = x.std(dim=(0,1),unbiased=False,keepdims=True)
     return gamma*(x-mean)/(var+eps)+beta
 
+def bn_forward(x, gamma, beta, eps=1e-5, momentum=0.1, training=True, running=None):
+    # x: (B,C,H,W)  gamma/beta: (C,)
+    B, C, H, W = x.shape
+    x_flat = x.transpose(1,0,2,3).reshape(C, -1)        # (C, BHW)
+    if training:
+        mean = x_flat.mean(axis=1, keepdims=True)       # (C,1)
+        var  = x_flat.var(axis=1, keepdims=True)
+        # 更新 running
+        running['mean'] = (1-momentum)*running['mean'] + momentum*mean.squeeze()
+        running['var']  = (1-momentum)*running['var']  + momentum*var.squeeze()
+    else:
+        mean = running['mean'].reshape(C,1)
+        var  = running['var'].reshape(C,1)
+    x_norm = (x_flat - mean) / np.sqrt(var + eps)
+    y = gamma.reshape(C,1) * x_norm + beta.reshape(C,1)
+    return y.reshape(C,B,H,W).transpose(1,0,2,3)
+
 if __name__ == "__main__":
     feature_num = 4
     batch_size = 2
@@ -37,3 +54,6 @@ if __name__ == "__main__":
     y = x.permute(0,2,1) #(B,C,L)
     res = model(y).permute(0,2,1)
     print(res)
+    # image = torch.randn(batch_size,3,128,128)
+    # y = bn_forward(image,1,0)
+    # print(y.shape)
